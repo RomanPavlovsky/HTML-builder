8efea34ed79d90e490c72d284b4da259a51c5581
Roman.P <killme15139214@gmail.com>
2023-05-08 02:16:42 +0300

    feat: completed 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyDir = async () => {
+  console.log(path.resolve(__dirname, 'files-copy'));
+  await fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), {
+    recursive: true,
+  });
+  let copyFiles = await fs.promises.readdir(
+    path.resolve(__dirname, 'files-copy')
+  );
+  for (const file of copyFiles) {
+    await fs.promises.unlink(path.resolve(__dirname, 'files-copy', file));
+  }
+  let sourceFiles = await fs.promises.readdir(path.resolve(__dirname, 'files'));
+  for (const file of sourceFiles) {
+    await fs.promises.copyFile(
+      path.resolve(__dirname, 'files', file),
+      path.resolve(__dirname, 'files-copy', file)
+    );
+  }
+};
+
+copyDir();
